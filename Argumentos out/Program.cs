a5338eca4fb3edfa26c1a21c2bdef7f5a66cba72
Kevin luan <cardosoluan3006@gmail.com>
2025-05-13 16:05:15 -0300

    Argumentos out

---
+++ Argumentos out/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Progrma
+{
+
+
+    static void Main()
+    {
+        int divid, divis, resul, res;
+
+
+        Console.WriteLine("Dirite o dividendo");
+        divid = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o Divisor");
+         divis = int.Parse(Console.ReadLine());
+         resul=Divide(divid,divis,out res);
+
+         Console.WriteLine("Dividendo/Divisor= "+ resul +"  o resto da divisão é:"+ res );
+         Console.WriteLine(res);
+
+    }
+
+    static int Divide(int divisor, int dividendo, out int resto)
+    {
+
+        int resultado;
+        resultado = divisor / dividendo;
+        resto = divisor % dividendo;
+        return resultado;
+
+    }
+
+}
